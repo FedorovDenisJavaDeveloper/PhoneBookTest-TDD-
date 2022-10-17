787f4710051475fc428202fcb0c1bc61f13b4bf1
Denis Fedorov <ssboshen@yandex.ru>
2022-10-17 15:30:15 +0300

    Add method implemented!

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class PhoneBook {
-    public int add(String name, String phone){
-        return 0;
+    TreeMap<String, String> phoneBook = new TreeMap<>();
+    HashMap<String, String> reversedKeyBook = new HashMap<>();
+
+    public int add(String name, String phone) {
+        if (name == null || phone == null) {
+            throw new NullPointerException();
+        }
+        if (phoneBook.putIfAbsent(name, phone) == null) {
+            reversedKeyBook.put(phone, name);
+        }
+        return phoneBook.size();
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,11 +1,9 @@
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
